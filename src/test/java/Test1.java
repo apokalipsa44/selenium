5a5b32df6bbcb9fb64c0db92e7402c7d962a65cb
apokalipsa44 <m_lachowicz@wp.pl>
2020-10-18 11:27:15 +0200

    first tests done

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,17 +1,32 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test1 {
+import java.util.concurrent.TimeUnit;
 
+public class Test1 {
+    WebDriver webDriver;
+
+    @BeforeEach
+    public void setupDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        webDriver.quit();
+    }
 
     @Test
-    public void test1(){
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe" );
-        WebDriver webDriver=new ChromeDriver();
-        webDriver.navigate().to("http://www.allegro.pl");
+    public void test1() {
 
-        webDriver.quit();
+        webDriver.navigate().to("http://przyklady.javastart.pl/jpetstore/");
+        webDriver.findElement(By.linkText("Enter the Store")).click();
+
 
     }
 
