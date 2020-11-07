a47f363aed28a018c3ac8adbfffc6dfd7b70702c
apokalipsa44 <m_lachowicz@wp.pl>
2020-11-07 12:02:32 +0100

    ranking done

--- src/main/java/com/michau/selenium/FirstRanking.java
+++ src/main/java/com/michau/selenium/FirstRanking.java
@@ -32,6 +32,46 @@ public class FirstRanking {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
+
+    public void ad4(){
+        final Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i=1; i < 11; i++) {
+            System.out.println(N+" x "+i+" = "+N*i);
+        }
+
+        scanner.close();
+    }
+
+    public void stringFormetter() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+
+    }
+
+    public void wielomians(){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j = 0; j < n; j++){
+                a = a + (int)Math.pow(2,j)*b;
+                System.out.print(a + " ");
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
 }
 
 
